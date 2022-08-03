39de054af48383184eade9392c0038f72ba589ce
arabking20 <ahmednasser.105@live.com>
2022-08-03 11:25:52 -0400

    created application context and modified url in json file

--- app/src/main/java/com/example/imagetest/MovieApplication.kt
+++ app/src/main/java/com/example/imagetest/MovieApplication.kt
@@ -1,7 +1,22 @@
 package com.example.imagetest
 
 import android.app.Application
+import android.content.Context
 
 class MovieApplication: Application() {
+
+    /**
+     * Initialize firebase, logger, analytics
+     * dependency injection.
+     *
+     */
+    override fun onCreate() {
+        super.onCreate()
+        movieApplication= applicationContext
+    }
+    companion object {
+        lateinit var movieApplication: Context
+    }
+
 
 }
--- app/src/main/java/com/example/imagetest/model/MovieResponse.kt
+++ app/src/main/java/com/example/imagetest/model/MovieResponse.kt
@@ -1,6 +1,7 @@
 package com.example.imagetest.model
 
-import java.time.Year
+import android.os.Parcel
+import android.os.Parcelable
 
 data class MovieResponse (
         val title: String,
@@ -8,4 +9,35 @@ data class MovieResponse (
         val rating: Double,
         val releaseYear: Int,
         val genre: List<String>
-        )
+        ): Parcelable {
+        constructor(parcel: Parcel) : this(
+                parcel.readString() ?: "",
+                parcel.readString()?: "",
+                parcel.readDouble(),
+                parcel.readInt(),
+                parcel.createStringArrayList()?.toList() ?: emptyList()
+        ) {
+        }
+
+        override fun writeToParcel(parcel: Parcel, flags: Int) {
+                parcel.writeString(title)
+                parcel.writeString(image)
+                parcel.writeDouble(rating)
+                parcel.writeInt(releaseYear)
+                parcel.writeStringList(genre)
+        }
+
+        override fun describeContents(): Int {
+                return 0
+        }
+
+        companion object CREATOR : Parcelable.Creator<MovieResponse> {
+                override fun createFromParcel(parcel: Parcel): MovieResponse {
+                        return MovieResponse(parcel)
+                }
+
+                override fun newArray(size: Int): Array<MovieResponse?> {
+                        return arrayOfNulls(size)
+                }
+        }
+}
