808db0a85888c5e9bc80e46a3a67bece92b2ec44
arabking20 <ahmednasser.105@live.com>
2022-08-03 11:27:26 -0400

    implemented partially landscape mode

--- app/src/main/java/com/example/imagetest/MainActivity.kt
+++ app/src/main/java/com/example/imagetest/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.imagetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import com.example.imagetest.model.view.FragementListMovie
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //super.getFragmentManager.beginTransaction()(R.id.container)
+
+        if (findViewById<FragmentContainerView>(R.id.container) !=null)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, FragementListMovie())
+
+            .commit()
     }
 }
